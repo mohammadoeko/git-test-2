6a730053762f88cc340419c2e76197ae77405394
Mohammad Eko <mohammadoeko@gmail.com>
2020-07-13 21:45:09 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Mencoba Git tes 2</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, sapiente iure pariatur sed itaque, delectus aliquam quis voluptatem porro quas explicabo libero hic provident vitae aut possimus ea similique facere!</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+        <li>Tiga</li>
+    </ul>
 </body>
 </html>
